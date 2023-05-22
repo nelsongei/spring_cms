3a54f480d71307e60f4a0e099035fa35da94c00e
nelsongei <psammy.ngei@gmail.com>
2023-05-22 17:31:56 +0300

    updated main class

--- src/main/java/com/nuke/Main.java
+++ src/main/java/com/nuke/Main.java
@@ -2,10 +2,51 @@ package com.nuke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+       SpringApplication.run(Main.class, args);
     }
+    @GetMapping("/greet")
+    public GreetResponse greet(){
+        return new GreetResponse("Hello");
+    }
+  //  record GreetResponse(String greet){}
+    class GreetResponse {
+        private final String greet;
+
+      GreetResponse(String greet) {
+          this.greet = greet;
+      }
+
+      public String getGreet() {
+          return greet;
+      }
+
+      @Override
+      public String toString() {
+          return "GreetResponse{" +
+                  "greet='" + greet + '\'' +
+                  '}';
+      }
+
+      @Override
+      public boolean equals(Object o) {
+          if (this == o) return true;
+          if (o == null || getClass() != o.getClass()) return false;
+          GreetResponse that = (GreetResponse) o;
+          return Objects.equals(greet, that.greet);
+      }
+
+      @Override
+      public int hashCode() {
+          return Objects.hash(greet);
+      }
+  }
 }
